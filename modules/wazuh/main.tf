872dec2cff4f7f5661040d0a00bd98389fe87838
app-yevgeni <23049337+yevgeni1980-max2006@users.noreply.github.com>
2026-08-16 15:30:56 +0300

    update

---
+++ modules/wazuh/main.tf
@@ -0,0 +1,34 @@
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+
+  set {
+    name  = "crds.enabled"
+    value = "true"
+  }
+}
+
+
+resource "helm_release" "wazuh" {
+  name             = "wazuh"
+  repository       = "https://morgoved.github.io/wazuh-helm/"
+  chart            = "wazuh"
+  namespace        = "wazuh"
+  create_namespace = true
+
+  wait    = true
+  timeout = 1800
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
